c2607458836371ef48f17a62951bcc6c0debad94
carlosbranga <loscarbala@gmail.com>
2026-03-19 21:56:50 -0300

    feat: layout de telas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Treino } from './pages/treino/treino';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Login },
   { path: 'cadastro', component: Register },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'treino', component: Treino },
 ];

b2c978e72f1b56d62dcebe1f71d4e54ee7491f4c
carlosbranga <loscarbala@gmail.com>
2026-05-17 23:50:35 -0300

    wip: integração com back e congifuração de versão

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { SocialAuthServiceConfig, GoogleLoginProvider } from '@abacritt/angularx-social-login';
 
 import { routes } from './app.routes';
 
@@ -7,6 +9,19 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('771239304883-c0jo8jkqn1c38qcdk4gm88d20pfohdio.apps.googleusercontent.com')
+          }
+        ]
+      } as SocialAuthServiceConfig,
+    }
   ]
 };
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,66 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { SocialAuthService, GoogleSigninButtonModule, GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { environment } from '../../../enviroments/enviroments';
+
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [RouterLink, GoogleSigninButtonModule],
   templateUrl: './login.html',
 })
-export class Login {}
+export class Login implements OnInit {
+
+  constructor(
+    private authService: SocialAuthService,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      if (user) {
+        console.log('Dados recebidos do Google:', user);
+        this.http.post(`${environment.apiUrl}/login/google`, { google_token: user.idToken })
+          .subscribe({
+            next: (resposta: any) => {
+              console.log('Sucesso! O Laravel autorizou:', resposta);
+              localStorage.setItem('token', resposta.access_token);
+
+              this.router.navigate(['/dashboard']);
+            },
+            error: (erro) => {
+              console.error('Erro ao validar login no Laravel:', erro);
+            }
+          });
+      }
+    });
+  }
+
+
+  fazerLoginNormal(emailDigitado: string, senhaDigitada: string) {
+    const credenciais = {
+      email: emailDigitado,
+      password: senhaDigitada
+    };
+
+    this.http.post(`${environment.apiUrl}/login`, credenciais)
+      .subscribe({
+        next: (resposta: any) => {
+          localStorage.setItem('token', resposta.access_token);
+          this.router.navigate(['/dashboard']);
+        },
+        error: (erro) => {
+          console.error('Credenciais inválidas:', erro);
+          alert('E-mail ou senha incorretos!');
+        }
+      });
+  }
+
+  entrarComGoogle() {
+    // Isso aqui força a abertura da janela do Google pelo seu botão customizado!
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+}
